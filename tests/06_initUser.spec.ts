047c5fc307e628081d77e482b0669ad37eb65d61
DAVID ZHANG <d12stan@hotmail.com>
2025-09-04 15:16:02 +0700

    feat: add 06_initUser.spec.ts

---
+++ tests/06_initUser.spec.ts
@@ -0,0 +1,60 @@
+import { workspace, Program } from "@coral-xyz/anchor";
+import { Marginfi } from "../target/types/marginfi";
+import { marginfiGroup, users } from "./rootHooks";
+import { Keypair, Transaction } from "@solana/web3.js";
+import { USER_ACCOUNT } from "./utils/mocks";
+import { accountInit } from "./utils/user-instructions";
+import { assert } from "chai";
+import { assertKeyDefault, assertKeysEqual, assertI80F48Equal, assertBNEqual } from "./utils/genericTests";
+
+describe("Initialize user account", () => {
+    const program = workspace.Marginfi as Program<Marginfi>;
+
+    it("(User 0) Initialize user account - happy path", async () => {
+        const accountKeypair = Keypair.generate();
+        const accountKey = accountKeypair.publicKey;
+        users[0].accounts.set(USER_ACCOUNT, accountKey);
+
+        let tx: Transaction = new Transaction();
+        tx.add(
+            await accountInit(program, {
+                marginfiGroup: marginfiGroup.publicKey,
+                marginfiAccount: accountKey,
+                authority: users[0].wallet.publicKey,
+                feePayer: users[0].wallet.publicKey,
+            })
+        );
+        await users[0].mrgnProgram.provider.sendAndConfirm(tx, [accountKeypair]);
+
+        const userAcc = await program.account.marginfiAccount.fetch(accountKey);
+        assertKeysEqual(userAcc.group, marginfiGroup.publicKey);
+        assertKeysEqual(userAcc.authority, users[0].wallet.publicKey);
+        const balances = userAcc.lendingAccount.balances;
+        for (let i = 0; i < balances.length; i++) {
+            assert.equal(balances[i].active, 0);
+            assertKeyDefault(balances[i].bankPk);
+            assertI80F48Equal(balances[i].assetShares, 0);
+            assertI80F48Equal(balances[i].liabilityShares, 0);
+            assertI80F48Equal(balances[i].emissionsOutstanding, 0);
+            assertBNEqual(balances[i].lastUpdate, 0);
+        }
+        assertBNEqual(userAcc.accountFlags, 0);
+    });
+
+    it("(user 1) Initialze user account - happy path", async () => {
+        const accountKeypair = Keypair.generate();
+        const accountKey = accountKeypair.publicKey;
+        users[1].accounts.set(USER_ACCOUNT, accountKey);
+
+        let tx: Transaction = new Transaction();
+        tx.add(
+            await accountInit(program, {
+                marginfiGroup: marginfiGroup.publicKey,
+                marginfiAccount: accountKey,
+                authority: users[1].wallet.publicKey,
+                feePayer: users[1].wallet.publicKey,
+            })
+        );
+        await users[1].mrgnProgram.provider.sendAndConfirm(tx, [accountKeypair]);
+    });
+});
