6e102788fd5f7feef0cfab9d544e43a6b9b519be
DAVID ZHANG <d12stan@hotmail.com>
2025-09-04 21:45:05 +0700

    feat: add 09_withdraw_repay.spec.ts

---
+++ tests/09_withdraw_repay.spec.ts
@@ -0,0 +1,443 @@
+import { workspace, Program, getProvider, AnchorProvider, BN } from "@coral-xyz/anchor";
+import { Marginfi } from "../target/types/marginfi";
+import { ecosystem, users, bankKeypairA, oracles, bankKeypairUsdc, verbose, bankKeypairSol } from "./rootHooks";
+import { USER_ACCOUNT } from "./utils/mocks";
+import { Transaction } from "@solana/web3.js";
+import { expectFailedTxWithError, getTokenBalance, assertKeysEqual } from "./utils/genericTests";
+import { composeRemainingAccounts, wrappedI80F48toBigNumber } from "@mrgnlabs/mrgn-common";
+import { borrowIx, depositIx, repayIx, withdrawEmissionsIx, withdrawIx } from "./utils/user-instructions";
+import { assert } from "chai";
+import { u64MAX_BN } from "./utils/types";
+
+describe("Withdraw funds", () => {
+    const program = workspace.Marginfi as Program<Marginfi>;
+    const provider = getProvider() as AnchorProvider;
+
+    const withdrawAmountTokenA = 0.1;
+    const withdrawAmountTokenA_native = new BN(
+        withdrawAmountTokenA * 10 ** ecosystem.tokenADecimals
+    );
+
+    const repayAmountUsdc = 0.1;
+    const repayAmountUsdc_native = new BN(
+        repayAmountUsdc * 10 ** ecosystem.usdcDecimals
+    );
+
+    it("(user 0) withdraws some token A - happy path", async () => {
+        const user = users[0];
+        const userAccKey = user.accounts.get(USER_ACCOUNT);
+
+        const bank = bankKeypairA.publicKey;
+        const bankBefore = await program.account.bank.fetch(bank);
+        const [userAccBefore, userTokenABefore, vaultTokenABefore] = await Promise.all([
+            program.account.marginfiAccount.fetch(userAccKey),
+            getTokenBalance(provider, user.tokenAAccount),
+            getTokenBalance(provider, bankBefore.liquidityVault),
+        ]);
+        const balancesBefore = userAccBefore.lendingAccount.balances;
+
+        await user.mrgnProgram.provider.sendAndConfirm(
+            new Transaction().add(
+                await withdrawIx(user.mrgnProgram, {
+                    marginfiAccount: userAccKey,
+                    bank: bank,
+                    tokenAccount: user.tokenAAccount,
+                    remaining: composeRemainingAccounts([
+                        [bankKeypairUsdc.publicKey, oracles.usdcOracle.publicKey],
+                        [bankKeypairA.publicKey, oracles.tokenAOracle.publicKey],
+                    ]),
+                    amount: withdrawAmountTokenA_native,
+                })
+            )
+        );
+
+        const bankAfter = await program.account.bank.fetch(bank);
+        const [userAccAfter, userTokenAAfter, vaultTokenAAfter] = await Promise.all(
+            [
+                program.account.marginfiAccount.fetch(userAccKey),
+                getTokenBalance(provider, user.tokenAAccount),
+                getTokenBalance(provider, bankAfter.liquidityVault),
+            ]
+        );
+        const balancesAfter = userAccAfter.lendingAccount.balances;
+        // Partial withdraw only, position is still open
+        assert.equal(bankAfter.lendingPositionCount, 1);
+
+        const withdrawExpected = withdrawAmountTokenA_native.toNumber();
+        if (verbose) {
+            console.log(
+                "User 0 withdraw " + withdrawAmountTokenA + " token A (" + withdrawExpected.toString() + ") native"
+            );
+        }
+
+        assert.equal(userTokenAAfter, userTokenABefore + withdrawExpected);
+        assert.equal(vaultTokenAAfter, vaultTokenABefore - withdrawExpected);
+
+        const sharesBefore = wrappedI80F48toBigNumber(
+            balancesBefore[0].assetShares
+        ).toNumber();
+        const sharesAfter = wrappedI80F48toBigNumber(
+            balancesAfter[0].assetShares
+        ).toNumber();
+        assert.equal(sharesAfter, sharesBefore - withdrawExpected);
+    });
+
+    it("(user 0) repays some USDC debt - happy path", async () => {
+        const user = users[0];
+        const userAccKey = user.accounts.get(USER_ACCOUNT);
+    
+        const bank = bankKeypairUsdc.publicKey;
+        const bankBefore = await program.account.bank.fetch(bank);
+        const [userAccBefore, userUsdcBefore, vaultUsdcBefore] = await Promise.all([
+          program.account.marginfiAccount.fetch(userAccKey),
+          getTokenBalance(provider, user.usdcAccount),
+          getTokenBalance(provider, bankBefore.liquidityVault),
+        ]);
+        const balancesBefore = userAccBefore.lendingAccount.balances;
+    
+        await user.mrgnProgram.provider.sendAndConfirm(
+          new Transaction().add(
+            await repayIx(user.mrgnProgram, {
+              marginfiAccount: userAccKey,
+              bank: bank,
+              tokenAccount: user.usdcAccount,
+              remaining: composeRemainingAccounts([
+                [bankKeypairUsdc.publicKey, oracles.usdcOracle.publicKey],
+                [bankKeypairA.publicKey, oracles.tokenAOracle.publicKey],
+              ]),
+              amount: repayAmountUsdc_native,
+            })
+          )
+        );
+    
+        const bankAfter = await program.account.bank.fetch(bank);
+        const [userAccAfter, userUsdcAfter, vaultUsdcAfter] = await Promise.all([
+          program.account.marginfiAccount.fetch(userAccKey),
+          getTokenBalance(provider, user.usdcAccount),
+          getTokenBalance(provider, bankAfter.liquidityVault),
+        ]);
+        // Partial repay only, still has debt
+        assert.equal(bankAfter.borrowingPositionCount, 1);
+        // Still has deposit in token A
+        assert.equal(bankAfter.lendingPositionCount, 1);
+        const balancesAfter = userAccAfter.lendingAccount.balances;
+    
+        const repayExpected = repayAmountUsdc_native.toNumber();
+        if (verbose) {
+          console.log(
+            "User 0 repaid " +
+              repayAmountUsdc +
+              " usdc (" +
+              repayExpected.toString() +
+              ") native"
+          );
+        }
+    
+        // user loses the USDC, the liquidity vault gains it....
+        assert.equal(userUsdcAfter, userUsdcBefore - repayExpected);
+        assert.equal(vaultUsdcAfter, vaultUsdcBefore + repayExpected);
+    
+        // User loses the liability shares of USDC...
+        // USDC has some borrows, so there is trivial interest here that affects accounting
+        const sharesBefore = wrappedI80F48toBigNumber(
+          balancesBefore[1].liabilityShares
+        ).toNumber();
+        const sharesAfter = wrappedI80F48toBigNumber(
+          balancesAfter[1].liabilityShares
+        ).toNumber();
+        assert.approximately(sharesAfter, sharesBefore - repayExpected, 1);
+    
+        // The bank has also lost the same amount of shares...
+        const bankSharesBefore = wrappedI80F48toBigNumber(
+          bankBefore.totalLiabilityShares
+        ).toNumber();
+        const bankSharesAfter = wrappedI80F48toBigNumber(
+          bankAfter.totalLiabilityShares
+        ).toNumber();
+        assert.approximately(bankSharesAfter, bankSharesBefore - repayExpected, 1);
+    });
+
+    it("(user 0) tries to repay all without claiming emissions - should fail", async () => {
+        const user = users[0];
+        const userAccKey = user.accounts.get(USER_ACCOUNT);
+        const bank = bankKeypairUsdc.publicKey;
+        
+        await expectFailedTxWithError(
+            async () => {
+                await user.mrgnProgram.provider.sendAndConfirm(
+                    new Transaction().add(
+                        await repayIx(user.mrgnProgram, {
+                            marginfiAccount: userAccKey,
+                            bank: bank,
+                            tokenAccount: user.usdcAccount,
+                            remaining: composeRemainingAccounts([
+                                [bankKeypairUsdc.publicKey, oracles.usdcOracle.publicKey],
+                                [bankKeypairA.publicKey, oracles.tokenAOracle.publicKey],
+                            ]),
+                            amount: u64MAX_BN,
+                            repayAll: true,
+                        })
+                    )
+                );
+            },
+            "CannotCloseOutstandingEmissions",
+            6033
+        );
+    });
+
+    it("(user 0) claims emissions (in token B) before repaying their balance - happy path", async () => {
+        const user = users[0];
+        const userAccKey = user.accounts.get(USER_ACCOUNT);
+        const bank = bankKeypairUsdc.publicKey;
+
+        const userBBefore = await getTokenBalance(provider, user.tokenBAccount);
+
+        await user.mrgnProgram.provider.sendAndConfirm(
+            new Transaction().add(
+                await withdrawEmissionsIx(user.mrgnProgram, {
+                    marginfiAccount: userAccKey,
+                    bank: bank,
+                    tokenAccount: user.tokenBAccount,
+                })
+            )
+        );
+
+        const userBAfter = await getTokenBalance(provider, user.tokenBAccount);
+        const diff = userBAfter - userBBefore;
+        if (verbose) {
+            console.log("Claimed Token B emissions: " + diff);
+        }
+
+        assert.ok(diff > 0);
+    });
+
+    it("(user 0) repays all of their USDC debt - happy path", async () => {
+        const user = users[0];
+        const userAccKey = user.accounts.get(USER_ACCOUNT);
+        const bank = bankKeypairUsdc.publicKey;
+
+        const bankBefore = await program.account.bank.fetch(bank);
+        const [userAccBefore, userUsdcBefore, vaultUsdcBefore] = await Promise.all([
+            program.account.marginfiAccount.fetch(userAccKey),
+            getTokenBalance(provider, user.usdcAccount),
+            getTokenBalance(provider, bankBefore.liquidityVault),
+        ]);
+        const balancesBefore = userAccBefore.lendingAccount.balances;
+        
+        // Actual should repay amount = liab shares * liab share value
+        const actualOwed = 
+            wrappedI80F48toBigNumber(balancesBefore[1].liabilityShares).toNumber();
+            wrappedI80F48toBigNumber(bankBefore.liabilityShareValue).toNumber();
+
+        await user.mrgnProgram.provider.sendAndConfirm(
+            new Transaction().add(
+                await withdrawEmissionsIx(user.mrgnProgram, {
+                    marginfiAccount: userAccKey,
+                    bank: bank,
+                    tokenAccount: user.tokenBAccount,
+                }),
+                await repayIx(user.mrgnProgram, {
+                    marginfiAccount: userAccKey,
+                    bank: bank,
+                    tokenAccount: user.usdcAccount,
+                    remaining: composeRemainingAccounts([
+                        [bankKeypairUsdc.publicKey, oracles.usdcOracle.publicKey],
+                        [bankKeypairA.publicKey, oracles.tokenAOracle.publicKey],
+                    ]),
+                    amount: u64MAX_BN,
+                    repayAll: true,
+                })
+            )
+        );
+
+        const bankAfter = await program.account.bank.fetch(bank);
+        const [userAccAfter, userUsdcAfter, vaultUsdcAfter] = await Promise.all([
+            program.account.marginfiAccount.fetch(userAccKey),
+            getTokenBalance(provider, user.usdcAccount),
+            getTokenBalance(provider, bankAfter.liquidityVault),
+        ]);
+        const balancesAfter = userAccAfter.lendingAccount.balances;
+
+        if (verbose) {
+            console.log("User 0 repaid entire USDC balance: ~" + actualOwed.toLocaleString());
+        }
+
+        // user loses the USDC, the liquidity vault gains it
+        assert.approximately(userUsdcAfter, userUsdcBefore - actualOwed, 2);
+        assert.approximately(vaultUsdcAfter, vaultUsdcBefore + actualOwed, 2);
+
+        // user loses the liab shares of USDC
+        const sharesBefore = wrappedI80F48toBigNumber(
+            balancesBefore[1].liabilityShares
+        ).toNumber();
+        const sharesAfter = wrappedI80F48toBigNumber(
+            balancesAfter[1].liabilityShares
+        ).toNumber();
+        assert.approximately(sharesAfter, sharesBefore - actualOwed, 2);
+        // This balances is now inactive
+        assert.equal(balancesAfter[1].active, 0);
+        assertKeysEqual(balancesAfter[0].bankPk, bankKeypairA.publicKey);
+
+        // The bank has also lost the same amount of shares
+        const bankSharesBefore = wrappedI80F48toBigNumber(
+            bankBefore.totalLiabilityShares
+        ).toNumber();
+        const bankSharesAfter = wrappedI80F48toBigNumber(
+            bankAfter.totalLiabilityShares
+        ).toNumber();
+        assert.approximately(bankSharesAfter, bankSharesBefore - actualOwed, 2);
+    });
+
+    it("(user 0) withdraws all token A balance - happy path", async () => {
+        const user = users[0];
+        const userAccKey = user.accounts.get(USER_ACCOUNT);
+        const bank = bankKeypairA.publicKey;
+
+        const bankBefore = await program.account.bank.fetch(bank);
+        const [userAccBefore, userTokenABefore, vaultTokenABefore] = await Promise.all([
+            program.account.marginfiAccount.fetch(userAccKey),
+            getTokenBalance(provider, user.tokenAAccount),
+            getTokenBalance(provider, bankBefore.liquidityVault),
+        ]);
+        const balancesBefore = userAccBefore.lendingAccount.balances;
+
+        const actualDeposited = 
+            wrappedI80F48toBigNumber(balancesBefore[0].assetShares).toNumber();
+            wrappedI80F48toBigNumber(bankBefore.assetShareValue).toNumber();
+
+        await user.mrgnProgram.provider.sendAndConfirm(
+            new Transaction().add(
+                await withdrawIx(user.mrgnProgram, {
+                    marginfiAccount: userAccKey,
+                    bank: bank,
+                    tokenAccount: user.tokenAAccount,
+                    remaining: [
+                        bankKeypairA.publicKey,
+                        oracles.tokenAOracle.publicKey,
+                        bankKeypairUsdc.publicKey,
+                        oracles.usdcOracle.publicKey,
+                    ],
+                    amount: withdrawAmountTokenA_native,
+                    withdrawAll: true,
+                })
+            )
+        );
+
+        const bankAfter = await program.account.bank.fetch(bank);
+        const [userAccAfter, userTokenAAfter, vaultTokenAAfter] = await Promise.all([
+            program.account.marginfiAccount.fetch(userAccKey),
+            getTokenBalance(provider, user.tokenAAccount),
+            getTokenBalance(provider, bankAfter.liquidityVault),
+        ]);
+        const balancesAfter = userAccAfter.lendingAccount.balances;
+        
+        const withdrawExpected = actualDeposited;
+        if (verbose) {
+            console.log("User 0 withdrew all token A balance: ~" + withdrawExpected.toLocaleString());
+        }
+
+        // user gains the token A, the liquidity vault loses it
+        assert.equal(userTokenAAfter, userTokenABefore + withdrawExpected);
+        assert.equal(vaultTokenAAfter, vaultTokenABefore - withdrawExpected);
+
+        // user loses the asset shares of token A
+        // since there hasn't been any interest (no token A borrowed), shares and Token A are 1:1
+        const sharesBefore = wrappedI80F48toBigNumber(
+            balancesBefore[0].assetShares
+        ).toNumber();
+        const sharesAfter = wrappedI80F48toBigNumber(
+            balancesAfter[0].assetShares
+        ).toNumber();
+        assert.equal(sharesAfter, sharesBefore - withdrawExpected);
+        // This balances is now inactive
+        assert.equal(sharesAfter, sharesBefore - withdrawExpected);
+        assert.equal(balancesAfter[0].active, 0);
+
+        // The bank has also lost the same amount of shares
+        const bankSharesBefore = wrappedI80F48toBigNumber(
+            bankBefore.totalAssetShares
+        ).toNumber();
+        const bankSharesAfter = wrappedI80F48toBigNumber(
+            bankAfter.totalAssetShares
+        ).toNumber();
+        assert.equal(bankSharesAfter, bankSharesBefore - withdrawExpected);
+    });
+
+    it("(user 1) withdraws all SOL balance - happy path", async () => {
+        // account to only have a single USDC deposit. We don't repeat the checks for exact numbers here though.
+        const user = users[1];
+        const userAccKey = user.accounts.get(USER_ACCOUNT);
+        const bank = bankKeypairSol.publicKey;
+
+        await user.mrgnProgram.provider.sendAndConfirm(
+            new Transaction().add(
+                await withdrawIx(user.mrgnProgram, {
+                    marginfiAccount: userAccKey,
+                    bank: bank,
+                    tokenAccount: user.wsolAccount,
+                    remaining: [bankKeypairUsdc.publicKey, oracles.usdcOracle.publicKey],
+                    amount: new BN(0),
+                    withdrawAll: true,
+                })
+            )
+        );
+
+        const bankAfter = await program.account.bank.fetch(bank);
+        const userAccAfter = await program.account.marginfiAccount.fetch(
+            userAccKey
+        );
+        const balancesAfter = userAccAfter.lendingAccount.balances;
+        assert.equal(bankAfter.lendingPositionCount, 0);
+
+        // This balance is now inactive
+        assert.equal(balancesAfter[1].active, 0);
+    });
+
+    it("(user 0) restores previous Token A deposits and USDC borrow", async () => {
+        const user = users[0];
+        const userAcc = user.accounts.get(USER_ACCOUNT);
+
+        const depositAmountA = 2;
+        const depositAmountA_native = new BN(
+            depositAmountA * 10 ** ecosystem.tokenADecimals
+        );
+
+        await user.mrgnProgram.provider.sendAndConfirm(
+            new Transaction().add(
+                await depositIx(user.mrgnProgram, {
+                    marginfiAccount: userAcc,
+                    bank: bankKeypairA.publicKey,
+                    tokenAccount: user.tokenAAccount,
+                    amount: depositAmountA_native,
+                })
+            )
+        );
+
+        const borrowAmountUsdc = 5;
+        const borrowAMountUsdc_native = new BN(
+            borrowAmountUsdc * 10 ** ecosystem.usdcDecimals
+        );
+
+        await user.mrgnProgram.provider.sendAndConfirm(
+            new Transaction().add(
+                await borrowIx(user.mrgnProgram, {
+                    marginfiAccount: userAcc,
+                    bank: bankKeypairUsdc.publicKey,
+                    tokenAccount: user.usdcAccount,
+                    remaining: composeRemainingAccounts([
+                        [bankKeypairA.publicKey, oracles.tokenAOracle.publicKey],
+                        [bankKeypairUsdc.publicKey, oracles.usdcOracle.publicKey],
+                    ]),
+                    amount: borrowAMountUsdc_native,
+                })
+            )
+        );
+
+        const userAccAfter = await program.account.marginfiAccount.fetch(userAcc);
+        let balances = userAccAfter.lendingAccount.balances;
+        assert.equal(balances[0].active, 1);
+        assertKeysEqual(balances[0].bankPk, bankKeypairA.publicKey);
+        assert.equal(balances[1].active, 1);
+        assertKeysEqual(balances[1].bankPk, bankKeypairUsdc.publicKey);
+    });
+})
